13315fdb7637fdf6700bd3c6bbc3f1241755e75f
sunandasharma61 <sunandasharma60101@gmail.com>
2023-02-17 21:56:13 +0530

    done

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import Expenses from './components/Expenses/Expenses';
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
-    title: 'book',
+    title: 'Book',
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
   { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: 'e3',
-    title: 'Car Insurance',
+    title: 'Notebook',
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//const mongoose=require("mongoose")
 
 import './index.css';
 import App from './App';
